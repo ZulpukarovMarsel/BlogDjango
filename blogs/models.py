490649fc0f4ce0a5365844c42b358c06ae1f5a9d
ZulpukarovMarsel <zulpukarovmarsel17@gmail.com>
2024-03-18 19:50:38 +0600

    Adding a pagination to the list view of posts

--- blogs/models.py
+++ blogs/models.py
@@ -38,5 +38,8 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('blogs:post_detail',
-                       args=[self.id])
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
 
