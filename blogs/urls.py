429e2dffc0893f6c6fa37d6a2b2273c7abead140
ZulpukarovMarsel <zulpukarovmarsel17@gmail.com>
2024-03-18 19:35:24 +0600

    modified the canonical URL of posts

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'blogs'
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('<int:id>/', views.post_detail, name='post_detail'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+         views.post_detail,
+         name='post_detail'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
