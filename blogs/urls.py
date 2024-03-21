3f73b6154bf2c2edc448d72ff817ba8d4e376fb9
ZulpukarovMarsel <zulpukarovmarsel17@gmail.com>
2024-03-21 21:45:30 +0600

    added tag

--- blogs/urls.py
+++ blogs/urls.py
@@ -6,8 +6,9 @@ from django.conf.urls.static import static
 app_name = 'blogs'
 
 urlpatterns = [
-    # path('', views.post_list, name='post_list'),
-    path('', views.PostListView.as_view(), name='post_list'),
+    path('', views.post_list, name='post_list'),
+    path('tag/<slug:tag__slug>/', views.post_list, name='post_list_by_tag'),
+    # path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
          views.post_detail,
          name='post_detail'),
